20f474541efdda2d79f60ac0bdebb69a227794a1
Samarth Gupta <samarthgupta1011@gmail.com>
2017-07-24 19:35:38 +0530

    UI Changes

--- app/src/main/java/com/halanx/tript/userapp/Activities/FavouritesActivity.java
+++ app/src/main/java/com/halanx/tript/userapp/Activities/FavouritesActivity.java
@@ -1,6 +1,8 @@
 package com.halanx.tript.userapp.Activities;
 
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,11 +14,20 @@ public class FavouritesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_favourites);
-//        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
-//        setSupportActionBar(myToolbar);
-//        ActionBar ab = getSupportActionBar();
-//        // Enable the Up button
-//        ab.setDisplayHomeAsUpEnabled(true);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("You have no favorites!");
+        builder.setMessage("Products added to favorites will be displayed here");
+        builder.setCancelable(false);
+        builder.setNegativeButton("Go back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        AlertDialog dial = builder.create();
+        dial.show();
+
     }
 }
